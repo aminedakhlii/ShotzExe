14f55e52ef76b5cd77b978f5a679427398e33414
Amine Dakhli <45517430+aminedakhlii@users.noreply.github.com>
2022-02-12 01:16:00 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Shotz Exe',
       theme: ThemeData(
         primarySwatch: Colors.blue,
